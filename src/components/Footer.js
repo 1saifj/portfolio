b9726f365d08c7d131432c70d2d8a5637b273c7e
werry216 <linodeBp29@gmail.com>
2022-01-22 10:39:15 +0200

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,70 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiFillSkype,
+} from "react-icons/ai";
+import { FaTelegram, FaDiscord } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="4" className="footer-copywright">
+          <h3> Designed and Developed by Aleksandr </h3>
+        </Col>
+        <Col md="4" className="footer-copywright">
+          <h3>Copyright © {year} AS</h3>
+        </Col>
+        <Col md="4" className="footer-body">
+          <ul className="footer-icons">
+            <li className="social-icons">
+              <a
+                href="https://github.com/werry216"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://join.skype.com/invite/DoiJVbqFRtUN"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillSkype />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://discord.gg/prSeNjQG"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaDiscord />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://t.me/bonanzalek"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaTelegram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
